2dd3348b81da64f0084bd9167abf3c22fc46c83c
SDMckone <seandmckone@gmail.com>
2023-06-06 21:05:38 -0400

    Finalize changes for today

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import loadNavBar from "./modules/navBar";
+import loadHomePage from "./modules/homePage";
+import loadMenuPage from "./modules/menuPage";
 
 const content = document.getElementById("content");
 
 content.appendChild(loadNavBar());
+// content.appendChild(loadHomePage());
+content.appendChild(loadMenuPage());
