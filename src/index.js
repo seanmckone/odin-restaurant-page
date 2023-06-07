83eb003d690eaee45ab019dca0fa79a5a0e4f207
SDMckone <seandmckone@gmail.com>
2023-06-07 19:53:20 -0400

    Finalize Project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import loadNavBar from "./modules/navBar";
 import loadHomePage from "./modules/homePage";
 import loadMenuPage from "./modules/menuPage";
+import loadAboutPage from "./modules/aboutPage";
 
 const content = document.getElementById("content");
 
 content.appendChild(loadNavBar());
-// content.appendChild(loadHomePage());
-content.appendChild(loadMenuPage());
+
+const homeButton = document.getElementById("home-button");
+const menuButton = document.getElementById("menu-button");
+const aboutButton = document.getElementById("about-button");
+
+content.appendChild(loadHomePage());
+let article = document.getElementsByTagName("article")[0];
+homeButton.style.color = "black";
+let currButton = homeButton;
+
+homeButton.addEventListener("click", function () {
+  currButton.style.color = "#9C9C9C";
+  content.removeChild(article)
+  content.appendChild(loadHomePage());
+  homeButton.style.color = "black";
+  article = document.getElementsByTagName("article")[0];
+  currButton = homeButton;
+});
+
+menuButton.addEventListener("click", function () {
+  currButton.style.color = "#9C9C9C";
+  content.removeChild(article)
+  content.appendChild(loadMenuPage());
+  menuButton.style.color = "black";
+  article = document.getElementsByTagName("article")[0];
+  currButton = menuButton;
+});
+
+aboutButton.addEventListener("click", function () {
+  currButton.style.color = "#9C9C9C";
+  content.removeChild(article)
+  content.appendChild(loadAboutPage());
+  aboutButton.style.color = "black";
+  article = document.getElementsByTagName("article")[0];
+  currButton = aboutButton;
+});
+
+
+
+// content.appendChild(loadMenuPage());
+// content.appendChild(loadAboutPage());
--- src/modules/navBar.js
+++ src/modules/navBar.js
@@ -7,27 +7,18 @@ function loadNavBar() {
   const navUl = document.createElement("ul");
 
   const homeButton = document.createElement("li");
-  homeButton.classList.add("home-button");
-  const homeLink = document.createElement("a");
-  homeLink.href = "";
-  homeLink.textContent = "Home";
-  homeButton.appendChild(homeLink);
+  homeButton.id = "home-button";
+  homeButton.textContent = "Home";
   navUl.appendChild(homeButton);
 
   const menuButton = document.createElement("li");
-  menuButton.classList.add("menu-button");
-  const menuLink = document.createElement("a");
-  menuLink.href = "";
-  menuLink.textContent = "Menu";
-  menuButton.appendChild(menuLink);
+  menuButton.id = "menu-button";
+  menuButton.textContent = "Menu";
   navUl.appendChild(menuButton);
 
   const aboutButton = document.createElement("li");
-  aboutButton.classList.add("about-button");
-  const aboutLink = document.createElement("a");
-  aboutLink.href = "";
-  aboutLink.textContent = "About";
-  aboutButton.appendChild(aboutLink);
+  aboutButton.id = "about-button";
+  aboutButton.textContent = "About";
   navUl.appendChild(aboutButton);
 
   const hr = document.createElement("hr");
